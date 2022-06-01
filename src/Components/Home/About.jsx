7adafb24509440bc07c41874fa6b601139765dbe
Aditya Mall <contact.adityamall@gmail.com>
2022-06-02 02:46:01 +0530

    added transition properties

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -5,21 +5,25 @@ const About = () => {
   return (
     <section
       id="about"
-      className="mt-11 grid grid-cols-1 gap-y-5 justify-items-center  md:grid-cols-2"
+      className="transition-all duration-200 ease-in-out mt-11 grid grid-cols-1 gap-y-5 justify-items-center  md:grid-cols-2"
     >
       {/* image container */}
-      <figure className="mr-8 overflow-hidden rounded-full w-[200px] h-[200px] md:w-full md:h-full md:rounded-none md:mr-0">
-        <img src={aboutImage} alt="about" className="w-full h-full object-contain object-right-bottom" />
+      <figure className="transition-all duration-200 ease-in-out mr-8 overflow-hidden rounded-full w-[200px] h-[200px] md:w-full md:h-full md:rounded-none md:mr-0">
+        <img
+          src={aboutImage}
+          alt="about"
+          className="transition-all duration-200 ease-in-out  w-full h-full object-contain object-right-bottom"
+        />
       </figure>
       {/* info container */}
-      <div className="flex flex-col items-center space-y-5 text-justify p-4">
-        <div className="flex justify-start">
-          <h1 className="font-bold font-poppins text-4xl py-1 border-b-4 border-black">
+      <div className="transition-all duration-200 ease-in-out flex flex-col items-center space-y-5 text-justify p-4">
+        <div className="transition-all duration-200 ease-in-out flex justify-start">
+          <h1 className="transition-all duration-200 ease-in-out font-bold font-poppins text-4xl py-1 border-b-4 border-black">
             About Me
           </h1>
           {/* <p>Aditya Mall</p> */}
         </div>
-        <div>
+        <div className="transition-all duration-200 ease-in-out">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
             vitae amet magnam earum, et impedit voluptates repudiandae!
@@ -28,9 +32,15 @@ const About = () => {
             consectetur adipisicing elit. Mollitia facere illum earum eaque
             alias maxime saepe reprehenderit unde nam praesentium?
           </p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure harum vitae, veniam quis in animi pariatur delectus rerum eveniet iusto, reiciendis, non atque adipisci? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quia ex sit voluptatum fugiat iure consequatur magni tempora porro architecto!</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure harum
+            vitae, veniam quis in animi pariatur delectus rerum eveniet iusto,
+            reiciendis, non atque adipisci? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Deleniti quia ex sit voluptatum fugiat
+            iure consequatur magni tempora porro architecto!
+          </p>
         </div>
-        <button className="p-4 bg-black text-white font-poppins font-bold  rounded-lg w-full">
+        <button className="transition-all duration-200 ease-in-out p-4 bg-black text-white font-poppins font-bold  rounded-lg w-full">
           resume
         </button>
       </div>
